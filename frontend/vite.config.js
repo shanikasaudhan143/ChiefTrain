feff3ab92e343d1bfa02168cd9b18ad712e6c075
shanikasaudhan143 <shanikasaudhan93@gmail.com>
2025-07-18 18:42:11 +0530

    added vite _redirect

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-});
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',  // 👈 this is key
+          dest: '.'                  // copy to dist/
+        }
+      ]
+    })
+  ]
+})
